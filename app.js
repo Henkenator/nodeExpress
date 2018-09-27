e71209f9410f333a321bd15b33c843c0c542e597
Henkenator <ljunggren.henrik@gmail.com>
2018-09-27 23:25:53 +0200

    passport

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 const sql = require('mssql');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -22,11 +26,16 @@ const config = {
 // sql.connect(config).catch(err => debug(err));
 
 app.use(morgan('tiny'));
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use((req, res, next) => {
     // debug('my middleware');
     next();
 });
+
+app.use(cookieParser());
+app.use(session({secret: 'library'}));
+require('./src/config/passport.js')(app);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
@@ -44,9 +53,11 @@ const nav =  [
 ];
 const bookRouter = require('./src/routes/bookRoutes')(nav);
 const adminRouter = require('./src/routes/adminRoutes')(nav);
+const authRouter = require('./src/routes/authRoutes')(nav);
 
 app.use('/books', bookRouter);
 app.use('/admin', adminRouter);
+app.use('/auth', authRouter);
 app.get('/', (req, res) => {
   // res.sendFile(path.join(__dirname, 'views', 'index.html'));
     res.render('index',
